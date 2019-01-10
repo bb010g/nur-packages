608c32283a1a9c5fa7f77360d1b99ee66438d343
Brayden Banks <me@bb010g.com>
2019-01-10 12:06:02 -0800

    reorganize

--- default.nix
+++ default.nix
@@ -5,9 +5,9 @@
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  dwdiff = pkgs.callPackage ./pkgs/dwdiff { };
-  ipscan = pkgs.callPackage ./pkgs/ipscan { };
-  just = pkgs.callPackage ./pkgs/just {  };
-  xcolor = pkgs.callPackage ./pkgs/xcolor { };
-  ydiff = pkgs.pythonPackages.callPackage ./pkgs/ydiff { };
+  dwdiff = pkgs.callPackage ./pkgs/tools/text/dwdiff { };
+  ipscan = pkgs.callPackage ./pkgs/applications/networking/ipscan { };
+  just = pkgs.callPackage ./pkgs/development/tools/build-managers/just {  };
+  xcolor = pkgs.callPackage ./pkgs/applications/graphics/xcolor { };
+  ydiff = pkgs.pythonPackages.callPackage ./pkgs/tools/text/ydiff { };
 }
