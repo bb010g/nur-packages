3f988bb7083fb2e1ae680b27f8a93d975c9c17b6
Brayden Banks <me@bb010g.com>
2019-02-13 20:28:06 -0800

    lz4json: init at v2

--- default.nix
+++ default.nix
@@ -10,6 +10,7 @@ in {
   dwdiff = pkgs.callPackage ./pkgs/tools/text/dwdiff { };
   ipscan = pkgs.callPackage ./pkgs/applications/networking/ipscan { };
   just = pkgs.callPackage ./pkgs/development/tools/build-managers/just {  };
+  lz4json = pkgs.callPackage ./pkgs/tools/compression/lz4json { };
   xcolor = pkgs.callPackage ./pkgs/applications/graphics/xcolor { };
   ydiff = pkgs.pythonPackages.callPackage ./pkgs/tools/text/ydiff { };
 }
