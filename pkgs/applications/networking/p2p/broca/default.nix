52c7e105ddfe5f071e16bdabb3fe4e6851657835
bb010g <me@bb010g.com>
2019-05-05 02:07:54 -0700

    broca-unstable, receptor-unstable: fix meta

--- pkgs/applications/networking/p2p/broca/default.nix
+++ pkgs/applications/networking/p2p/broca/default.nix
@@ -31,8 +31,16 @@ buildPythonApplication rec {
   meta = with stdenv.lib; {
     description =
       "Bittorrent RPC proxy between Transmission clients and Synapse servers";
+    longDescription = ''
+      This is a proxy server that translates Synapse RPC into Transmission RPC.
+
+      To use it, run the daemon and point your Transmission client at it. Set
+      the username to your desired Synapse RPC URI (e.g. ws://localhost:8412)
+      and the password to your Synapse RPC password, and the host to
+      http://localhost:9091.
+    '';
     homepage = https://broca.synapse-bt.org/;
-    downloadPage = https://git.sr.ht/~sircmpwn/broca;
+    downloadPage = https://github.com/ddevault/broca;
     license = with licenses; bsd3;
     maintainers = with maintainers; [ bb010g ];
   };
