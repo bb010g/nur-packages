38ddf9388d328fb131684ca3d0e24e796f36c37d
bb010g <me@bb010g.com>
2020-02-22 20:40:57 -0800

    Actually fix libs evaluation
    This structure does not allow for what I was trying for. The external
    fixed-point passed back in to the module attribute disallows optionally
    returning a bare version.
    
    Maybe I'll change this in the future.

--- lib/default.nix
+++ lib/default.nix
@@ -1,33 +1,17 @@
 lib: libSuper:
 
 let
-  inherit (builtins) isAttrs mapAttrs typeOf;
-  mergeConst = merge: x: y:
-    y;
-  mergeAttrs = merge: x: y:
-    x // (mapAttrs (n: yV: if x ? ${n} then merge x.${n} yV else yV) y);
-  merge' = merge: x: y:
-    let xTy = typeOf x; yTy = typeOf y; in
-    if xTy != yTy
-      then merge x y
-    else if xTy == "set"
-      then mergeAttrs merge x y
-    else
-      merge x y;
-  merge_0 = mergeConst merge_0;
-  merge_1 = merge' merge_0;
-  merge_2 = merge' merge_1;
-  libMerged = merge_2 libSuper lib;
-  callLib = file: import file libMerged libSuper;
+  callLibs = name: file:
+    libSuper.${name} // import file { lib = lib; libSuper = libSuper; };
 in let exports = {
 
   ## modules
 
-  attrsets = callLib ./attrsets.nix;
+  attrsets = callLibs "attrsets" ./attrsets.nix;
   # edn = import ./edn;
-  fixedPoints = callLib ./fixed-points.nix;
-  lists = callLib ./lists.nix;
-  trivial = callLib ./trivial.nix;
+  fixedPoints = callLibs "fixedPoints" ./fixed-points.nix;
+  lists = callLibs "lists" ./lists.nix;
+  trivial = callLibs "trivial" ./trivial.nix;
   # utf8 = import ./utf-8;
 
   ## top-level
